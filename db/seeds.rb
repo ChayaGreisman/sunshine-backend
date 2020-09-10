ac5bdd1f4ae4c6019fdd92cb8780a7c71adbee9d
ChayaGreisman <chayamgreisman@gmail.com>
2020-09-09 21:02:18 -0400

    small changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ Toy.delete_all
 Child.delete_all
 
 
-sam = Child.create(name: "Sam Green", img_url:"https://media-private.canva.com/7yyi4/MAEBe27yyi4/1/tl.png?response-expires=Fri%2C%2010%20Jul%202020%2013%3A41%3A39%20GMT&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T113632Z&X-Amz-SignedHeaders=host&X-Amz-Expires=7506&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=2fa893d75f4d62962c6d305eee247342f9b0a030569899a59cac437499a4c51c", parent_email: "chayamgreisman@gmail.com", potty_trained: true, birthday: "2016-03-25")
-lily = Child.create(name: "Lily Spiro", img_url:"https://media-private.canva.com/CLmhU/MAEBfBCLmhU/1/tl.png?response-expires=Fri%2C%2010%20Jul%202020%2013%3A54%3A33%20GMT&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T113632Z&X-Amz-SignedHeaders=host&X-Amz-Expires=8280&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=c1908bd5f741f8c5e1c651a4c43d40278d621ac7c32fcaa38d071df15a33316e", parent_email: "chayamgreisman@gmail.com", potty_trained: false, birthday: "2018-07-01")
+sam = Child.create(name: "Sam Green", img_url:"./assets/sam.png", parent_email: "lily_jackson's_mom@gmail.com", potty_trained: true, birthday: "2016-03-25")
+lily = Child.create(name: "Lily Jackson", img_url:"./assets/lily.png", parent_email: "lily_jackson's_mom@gmail.com", potty_trained: false, birthday: "2018-07-01")
 
-Toy.create(name: "Giraffe", img_url: "https://media-private.canva.com/QKXvI/MAEBfDQKXvI/1/t.png?response-expires=Fri%2C%2010%20Jul%202020%2003%3A48%3A01%20GMT&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T005023Z&X-Amz-SignedHeaders=host&X-Amz-Expires=10657&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=3d01d2fb68e34a6c1d69181dd6897cda203172dd8b6fd593756bd9b6fbdc5c0f", child_id: sam.id)
-Toy.create(name: "Bear", img_url: "https://media-private.canva.com/B-0-k/MAEBfBB-0-k/1/t.png?response-expires=Fri%2C%2010%20Jul%202020%2003%3A50%3A17%20GMT&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T005023Z&X-Amz-SignedHeaders=host&X-Amz-Expires=10793&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=f73604aaf42d5d05254f75f1461aee98111473c40299caa2af8b369a77c49629", child_id: sam.id)
-Toy.create(name: "Doll", img_url: "https://media-private.canva.com/7HDGk/MAEBfJ7HDGk/1/tl.png?response-expires=Fri%2C%2010%20Jul%202020%2014%3A22%3A07%20GMT&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T113632Z&X-Amz-SignedHeaders=host&X-Amz-Expires=9934&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=caa507d5bdbb6d94740e21d052e77a04abebdbce9883d6ac561af1dcb8340164", child_id: lily.id)
+Toy.create(name: "Giraffe", img_url: "./assets/giraffe.png", child_id: sam.id)
+Toy.create(name: "Bear", img_url: "./assets/bear.png", child_id: sam.id)
+Toy.create(name: "Doll", img_url: "./assets/doll.png", child_id: lily.id)
 
 Allergy.create(name: "peanuts", severe: true, child_id: sam.id)
 Allergy.create(name: "soy", severe: true, child_id: sam.id)
